8834af574428ba4a92a0012a29288d05f8a0d085
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-08-21 13:49:18 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,22 +10,24 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-blue-500' />
+          <div className='w-5 h-5 rounded-full bg-blue-300' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-blue-500'>Kannemeyer</span>
+            Hi, I'm <span className='text-blue-300'>Kannemeyer</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a full stack developer<br className='sm:block hidden' />
-            I create websites,web apps and applications.
+            I create websites, web apps and applications.
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
+<div className="hidden md:block h-full	">
+<ComputersCanvas />
+</div>
+  
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
