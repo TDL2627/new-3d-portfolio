5c10e757fb4a3d9a8062aedb5a547e1ca1e13213
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-09-14 15:58:48 +0200

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -159,33 +159,33 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "After Kannemeyer optimized our website, our traffic increased by 50%. We can't thank them enough!.",
+    name: "Alex Sexwale",
+    designation: "Senior developer",
+    company: "WBWR",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+      "I've never met a web developer who truly cares about their clients' success like he does.",
+    name: "Lynn Boyce",
+    designation: "Project Manager",
+    company: "WBWR",
+    image: "https://randomuser.me/api/portraits/women/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+      " I thought it was impossible to make an app as beautiful as our product, but he proved me wrong.",
+    name: "Mark Stone",
+    designation: "Developer",
+    company: "Ayoba",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Unitech",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
@@ -206,7 +206,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Tuck Shop Finder",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
@@ -227,7 +227,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Small Shop",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
