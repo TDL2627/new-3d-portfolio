8744811a93e77d049e58da798d1f5d50cc72cd36
TDL2627 <the.dark.lord.2627@gmail.com>
2024-02-09 12:15:55 +0200

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,13 @@ const App = () => {
           <Contact />
           <StarsCanvas />
         </div>
+        <footer className="bg-black py-4 text-white">
+        <div className="container mx-auto text-center">
+          <p>&copy; {new Date().getFullYear()} Portfolio. All rights reserved.</p>
+          <p className="mt-2"> Cape Town, South Africa</p>
+          <a target="_blank" className="mt-4 underline text-right" href={"https://github.com/TDL2627"}>Created by TDL2627</a>
+        </div>
+      </footer>
       </div>
     </BrowserRouter>
   );
